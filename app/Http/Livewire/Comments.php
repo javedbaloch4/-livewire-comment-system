aa490462d8212ca033b19dcc715b732be874c47a
Javed Baloch <mjavedahmed@live.com>
2020-08-30 02:02:48 +0500

    Flash Messages & Pagination Added

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -5,20 +5,17 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use Carbon\Carbon;
 use App\Comment;
+use Livewire\WithPagination;
 
 class Comments extends Component
 {
+    use WithPagination;
 
     public $newComment = '';
 
-    public $comments;
-
     public function mount() {
 
-        $initialComments = Comment::latest()->get();
-        $this->comments = $initialComments;
     }
-
 
     public function addComment() {
       
@@ -31,16 +28,15 @@ class Comments extends Component
             'user_id' => 1
         ]);
 
-        $this->comments->prepend($createdComment);
-
         $this->newComment = "";
-        
+        session()->flash('message', 'Comment has been added.');
     }
 
     public function remove($id) {
         // $this->comments = $this->comments->where('id', '!=', $id); // Comment Collection
-        $this->comments = $this->comments->except($id);
         $comment = Comment::destroy($id);
+        session()->flash('message','Comment has been deleted.');
+
     }
 
     public function updated($field) {
@@ -53,7 +49,9 @@ class Comments extends Component
 
     public function render()
     {
-        return view('livewire.comments');
+        return view('livewire.comments', [
+            'comments' => Comment::paginate(2)
+        ]);
     }
 
 }
